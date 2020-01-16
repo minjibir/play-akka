198957fd5b742ab9035a4feee59cbd9ed60e528e
Jabir Minjibir <jabir.minjibir@gmail.com>
2020-01-16 23:16:11 +0100

    Removed persistence query conf

--- app/actors/CustomerActor.scala
+++ app/actors/CustomerActor.scala
@@ -3,6 +3,10 @@ package actors
 import akka.actor.ActorLogging
 import akka.persistence.PersistentActor
 
+case class CustomerState(customers: List[Customer] = List.empty[Customer]) {
+  def updated(evt: Evt): CustomerState = copy(evt.data :: events)
+}
+
 class CustomerActor extends PersistentActor with ActorLogging{
 
   override def persistenceId: String = "customers"
